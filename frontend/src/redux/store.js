5121475b21d8895ff1de9ba3c36c9863957c019e
Harshavardhana <vardhanun333@gmail.com>
2024-04-18 18:50:03 +0530

    movies backend

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,17 @@
+import { configureStore } from "@reduxjs/toolkit";
+import { setupListeners } from "@reduxjs/toolkit/query/react"; //is used to set up a listener for Redux Query.
+import { apiSlice } from "./api/apiSlice";
+import authReducer from "./features/auth/authSlice";
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+    auth: authReducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(apiSlice.middleware),
+  devTools: true,
+});
+
+setupListeners(store.dispatch); //listening to store
+export default store;
