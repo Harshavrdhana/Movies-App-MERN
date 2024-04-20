7902ba16a0887626bd522086c9a15ec9745a9b76
Harshavardhana <vardhanun333@gmail.com>
2024-04-20 15:30:19 +0530

    header and homescreens(frontend)

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,16 @@
-import Header from "./Movies/Header"
+import Header from "./Movies/Header";
+import MoviesContainerPage from "./Movies/MoviesContainerPage";
 
 const Home = () => {
   return (
     <>
-      <Header/>
-    </>
-  )
-}
+      <Header />
 
-export default Home
+      <section className="mt-[10rem]">
+        <MoviesContainerPage />
+      </section>
+    </>
+  );
+};
+
+export default Home;
